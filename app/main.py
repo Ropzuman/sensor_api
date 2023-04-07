a9cc200c7bc477455e64bf6c57bed2fa831e5cf1
Roope Vähä-Aho <roope.vahaaho@gmail.com>
2023-04-07 22:03:00 +0300

    rungon luonti aloitettu omaan oksaan

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from .database import models
+from .database.database import engine
+from .routers import measurements, sensors
+
+models.Base.metadata.create_all(bind=engine)
+
+app=FastAPI()
+
+app.include_router(sensors.router)
+app.include_router(measurements.router)
