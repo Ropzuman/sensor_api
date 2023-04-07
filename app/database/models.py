8f2ef1953e3d614b7cb370c3f42b3935fc53037d
Roope Vähä-Aho <roope.vahaaho@gmail.com>
2023-04-08 00:34:01 +0300

    modelit v1.0

--- app/database/models.py
+++ app/database/models.py
@@ -3,11 +3,23 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
-# class Sensor(Base):
-#     __tablename__ = "sensors"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String, unique=True, index=True)
-#     description = Column(String)
-#     unit = Column(String)
-#     measurements = relationship("Measurement", back_populates="sensor")
+class Sensor(Base):
+    __tablename__ = "sensors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    section = Column(String, unique=True, index=True)
+    status = Column(String)
+
+    measurements = relationship("Measurement", back_populates="sensor")
+
+
+class Measurement(Base):
+    __tablename__ = "measurements"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sensor_id = Column(Integer, ForeignKey("sensors.id"))
+    value = Column(Integer)
+    timestamp = Column(String)
+
+    sensor = relationship("Sensor", back_populates="measurements")
