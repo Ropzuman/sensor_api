38ce01e99ae5d63ff78944f3abdc0ceb76779b17
Roope Vähä-Aho <roope.vahaaho@gmail.com>
2023-04-08 17:10:16 +0300

    endpointtien ja skeemojen hiontaa

--- app/database/sensors_crud.py
+++ app/database/sensors_crud.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from . import models
-from .schemas import SensorBase, SensorDB
+from .schemas import SensorBase, SensorData, SensorDB
 
 
 def get_all_sensors(db: Session):
@@ -16,8 +16,10 @@ def read_sensor_by_id(db: Session, sensor_id: int):
     return sensor
 
 
-def read_sensor_by_section(db: Session, section: str):
-    sensor = db.query(models.Sensor).filter(models.Sensor.section == section).first()
+def read_sensor_by_section(db: Session, sensor_section: str):
+    sensor = (
+        db.query(models.Sensor).filter(models.Sensor.section == sensor_section).all()
+    )
     if sensor is None:
         raise HTTPException(status_code=404, detail="Sensor not found")
     return sensor
@@ -29,3 +31,11 @@ def create_sensor(db: Session, sensor: SensorBase):
     db.commit()
     db.refresh(sensor)
     return sensor
+
+
+def add_measurement(db: Session, id: int, measurement: SensorData):
+    rel = models.Measurement(**measurement.dict(), sensor_id=id)
+    db.add(rel)
+    db.commit()
+    db.refresh(rel)
+    return rel
--- app/routers/sensors.py
+++ app/routers/sensors.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ..database.database import get_db
-from ..database.schemas import AllSensors, SensorBase, SensorData, SensorDB
+from ..database.schemas import AllSensors, DataDB, SensorBase, SensorData, SensorDB
 from ..database.sensors_crud import (
+    add_measurement,
     create_sensor,
     get_all_sensors,
     read_sensor_by_id,
@@ -14,8 +15,8 @@ router = APIRouter(prefix="/Sensors")
 
 
 @router.get("", response_model=AllSensors)
-def read_sensors(id: int = 0, db: Session = Depends(get_db)):
-    if id != 0:
+def read_sensors(id: int, db: Session = Depends(get_db)):
+    if id == int:
         return read_sensor_by_id(db, id)
     return get_all_sensors(db)
 
@@ -35,6 +36,8 @@ def create_sensors(sensor: SensorBase, db: Session = Depends(get_db)):
     return create_sensor(db, sensor)
 
 
-@router.post("/{id}", response_model=SensorDB)
-def add_measurement(id: int, measurement: SensorData, db: Session = Depends(get_db)):
-    return add_measurement(id, measurement, db)
+@router.post("/{id}", response_model=DataDB)
+def add_measurement_for_sensor(
+    id: int, measurement: SensorData, db: Session = Depends(get_db)
+):
+    return add_measurement(db, id, measurement)
