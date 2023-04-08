d825234a28fefd0573c4e50d94829db3db7d867f
Roope Vähä-Aho <roope.vahaaho@gmail.com>
2023-04-08 14:22:53 +0300

    lisäyksiä

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -1,1 +1,34 @@
+import datetime
+
 from pydantic import BaseModel
+
+
+class SensorBase(BaseModel):
+    section: str
+    status: str
+
+    class Config:
+        orm_mode = True
+
+
+class SensorData(BaseModel):
+    timestamp: datetime.datetime
+    temperature: int
+
+    class Config:
+        orm_mode = True
+
+
+class SensorDB(SensorBase):
+    id: int
+    measurements: list[SensorData]
+
+    class Config:
+        orm_mode = True
+
+
+class AllSensors(BaseModel):
+    sensors: list[SensorDB]
+
+    class Config:
+        orm_mode = True
